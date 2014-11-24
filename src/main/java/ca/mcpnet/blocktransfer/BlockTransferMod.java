f233033d65ce0204eff0d87ff139bbb13e8c6a8e
Michael Gentili <gentili@mcpnet.ca>
2014-11-24 17:13:50 -0500

    Version update 1.1.0

--- src/main/java/ca/mcpnet/blocktransfer/BlockTransferMod.java
+++ src/main/java/ca/mcpnet/blocktransfer/BlockTransferMod.java
@@ -45,7 +45,7 @@ public class BlockTransferMod
 {
     public static final String MODID = "blocktransfer";
     public static final String MODNAME = "Block Transfer Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.0";
     
     @Instance(value = BlockTransferMod.MODID)
     public static BlockTransferMod instance;
